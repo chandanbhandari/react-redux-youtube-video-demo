bb887c68cee737d3631f18ded6ad8d2cda53e2ba
chandan <chandanbhandari24@gmail.com>
2018-03-05 16:44:03 +0530

    youtube search api added

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import SearchBar from "./search_bar";
+
+import YTSearch from 'youtube-api-search'
+
+const API_KEY = "AIzaSyAbxuztwjaakfGa5L4xb38SEW2r2a0yVak";
+
+
 
 export default class App extends Component {
+
+  constructor(props){
+      super(props);
+      this.state = { videos : [] }
+
+      YTSearch({key : API_KEY , term :'surfboard'} , videos => this.setState({videos}))
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <SearchBar/>
     );
   }
 }
